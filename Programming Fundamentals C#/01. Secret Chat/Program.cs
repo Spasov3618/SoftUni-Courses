adf5ce22bbcce321d60baab2a9491b41e4c6e683
Spasov3618 <svetlio_spasov@abv.bg>
2022-03-22 11:49:58 +0200

    Add Problem for Exam

---
+++ Programming Fundamentals C#/01. Secret Chat/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01._Secret_Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //На първия ред на входа ще получите скритото съобщение.След това, докато не бъде дадена командата "Разкриване", ще получите низове с инструкции за различни операции, които трябва да бъдат извършени върху скритото съобщение, за да го интерпретирате и разкриете действителното му съдържание.Има няколко типа инструкции, разделени от ":|:"
+            //• "InsertSpace:|:{index}":
+            //o Вмъква единичен интервал в дадения индекс.Посоченият индекс винаги ще бъде валиден.
+            //• "Обратно:|:{подниз}":
+            //o Ако съобщението съдържа дадения подниз, изрежете го, обърнете го и го добавете в края на съобщението.
+            //o Ако не, отпечатайте "error".
+            //o Тази операция трябва да замени само първото появяване на дадения подниз, ако има две или повече поява.
+            //• "Промяна на всички:|:{подниз}:|:{замяна}":
+            //o Променя всички срещания на дадения подниз със заместващия текст.
+            //Вход / Ограничения
+            //• На първия ред ще получите низ със съобщение.
+            //• На следните редове ще получавате команди, разделени от ":|:".
+            //Изход
+            //• След всеки набор от инструкции отпечатайте получения низ.
+            //• След като бъде получена командата "Разкриване", отпечатайте това съобщение:
+            //„Имате ново текстово съобщение: { message}“
+            string input = Console.ReadLine();
+            string command;
+            while ((command = Console.ReadLine()) != "Reveal")
+            {
+                string[] arg = command.Split(":|:", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string action = arg[0];
+                if (action == "InsertSpace")
+                {
+                    int index = int.Parse(arg[1]);
+                    input = input.Insert(index, " ");
+                    Console.WriteLine(input);
+                }
+
+                else if (action == "Reverse")
+                {
+                    if (!input.Contains(arg[1]))
+                    {
+                        Console.WriteLine("error");
+
+                    }
+                    else
+                    {
+                        string search = arg[1];
+                        var reversedSubstr = string.Empty;
+                        var index = input.IndexOf(search);
+
+                        input = input.Remove(index, search.Length);
+
+                        for (int i = search.Length - 1; i >= 0; i--)
+                        {
+                            reversedSubstr += search[i];
+                        }
+
+                        input = input.Insert(input.Length, reversedSubstr);
+                        Console.WriteLine(input);
+                    }
+                }
+                else if (action == "ChangeAll")
+                {
+                    string oldsymbol = arg[1];
+                    string newsymbol = arg[2];
+                    input = input.Replace(oldsymbol, newsymbol);
+                    Console.WriteLine(input);
+                }
+            }
+            Console.WriteLine($"You have a new text message: {input}");
+        }
+    }
+}
---
+++ Programming Fundamentals C#/03. Need for Speed III/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Need_for_Speed_III
+{
+    class Car
+    {
+        public Car(string model,int km, int fuel)
+        {
+            Model = model;
+            Km = km;
+            Fuel = fuel;
+        }
+        public string Model { get; set; }
+        public int Km { get; set; }
+        public int Fuel { get; set; }  
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numCars = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < numCars; i++)
+            {
+                string[] newcars = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string model = newcars[0];
+                int km = int.Parse(newcars[1]);
+                int fuel = int.Parse(newcars[2]);
+                Car newmodel = new Car(model, km, fuel);
+                cars.Add(newmodel);
+            }
+            string command;
+            while ((command = Console.ReadLine()) != "Stop")
+            {
+                string[] arg = command.Split(" : ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                if (arg[0] == "Drive")
+                {
+                    string model = arg[1];
+                    int km = int.Parse(arg[2]);
+                    int fuel = int.Parse(arg[3]);
+                    Car search = cars.FirstOrDefault(x => x.Model == model);
+                    if (search.Fuel >= fuel)
+                    {
+                        search.Km += km;
+                        search.Fuel -= fuel;
+                        Console.WriteLine($"{model} driven for {km} kilometers. {fuel} liters of fuel consumed.");
+                        if (search.Km >= 100000)
+                        {
+                            cars.Remove(search);
+                            Console.WriteLine($"Time to sell the {model}!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not enough fuel to make that ride");
+                    }
+                }
+                else if (arg[0] == "Refuel")
+                {
+                    string model = arg[1];
+                    int fuel = int.Parse(arg[2]);
+                    Car search = cars.FirstOrDefault(x => x.Model == model);
+                    if (search.Fuel + fuel > 75)
+                    {
+                        int refuel = 75 - search.Fuel;
+                        search.Fuel += refuel;
+                        Console.WriteLine($"{model} refueled with {refuel} liters");
+                    }
+                    else
+                    {
+                        search.Fuel += fuel;
+                        Console.WriteLine($"{model} refueled with {fuel} liters");
+                    }
+                }
+                else if (arg[0] == "Revert")
+                {
+                    string model = arg[1];
+                    int km = int.Parse(arg[2]);
+                    Car search = cars.FirstOrDefault(x => x.Model == model);
+                    if (search.Km - km < 10000)
+                    {
+                        search.Km = 10000;
+                    }
+                    else
+                    {
+                        search.Km -= km;
+                        Console.WriteLine($"{model} mileage decreased by {km} kilometers");
+                    }
+
+                }
+
+            }
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} -> Mileage: {car.Km} kms, Fuel in the tank: {car.Fuel} lt.");
+            }
+        }      
+    }
+}
